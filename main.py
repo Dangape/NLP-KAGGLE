baf4be718d1374910d596410d2d3ee4de39911d4
Daniel Aben-Athar Bemerguy <daniel06197@gmail.com>
2021-01-22 23:12:28 -0300

    loading datasets

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 # Created by: Daniel Bemerguy 
 # 22/01/2021 at 23:01
 import pandas as pd
+import numpy as np
+
+#Import train and test dataset
+train = pd.DataFrame(pd.read_csv('data/train.csv',sep = ','))
+test = pd.DataFrame(pd.read_csv('data/test.csv',sep = ','))
+
