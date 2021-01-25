dd800e85da306b0b334e065b0d2e453bc722fb05
Daniel Aben-Athar Bemerguy <daniel06197@gmail.com>
2021-01-25 00:22:54 -0300

    SGD classifier

--- generate_submission.py
+++ generate_submission.py
@@ -19,19 +19,37 @@ test_df = pd.DataFrame(pd.read_csv('data/test.csv',sep = ',')) #load test data
 
 #test_df['processed_tweet'] = test_df['text'].apply(lambda x:remove_stop_words(x))
 
-def create_word_features(words):
-    useful_words = [word for word in words if word not in stopwords.words("english")]
-    my_dict = dict([(word, True) for word in useful_words if word in useful_words])
-    return my_dict
+########################
+#Naive Bayes
+# def create_word_features(words):
+#     useful_words = [word for word in words if word not in stopwords.words("english")]
+#     my_dict = dict([(word, True) for word in useful_words if word in useful_words])
+#     return my_dict
+#
+# test_df["processed_tweet"] = test_df["text"].apply(lambda x:tknzr.tokenize(x))
+# test_df["processed_tweet"] = test_df["processed_tweet"].apply(lambda x:create_word_features(x))
+# test_df["target"] = test_df["processed_tweet"].apply(lambda x:loaded_model.classify(x))
+#
+# #print(test_df.head())
+#
+# submission =test_df.loc[:,['id','target']]
+# # print(len(submission))
+# print(submission)
+###############################
+#SGD
+def remove_stop_words(tweet):
+    tokenized_tweet = tknzr.tokenize(tweet)
+    processed_tweet = list(word for word in tokenized_tweet if word not in stop_words)
+    processed_tweet = ' '.join(processed_tweet)
+    return processed_tweet
 
-test_df["processed_tweet"] = test_df["text"].apply(lambda x:tknzr.tokenize(x))
-test_df["processed_tweet"] = test_df["processed_tweet"].apply(lambda x:create_word_features(x))
-test_df["target"] = test_df["processed_tweet"].apply(lambda x:loaded_model.classify(x))
-
-print(test_df.head())
+test_df['processed_tweet'] = test_df['text'].apply(lambda x:remove_stop_words(x))
+pred =  loaded_model.predict(test_df.loc[:,'processed_tweet'])
+for i in range(0,len(test_df)):
+    test_df.loc[i,'target'] = pred[i]
 
 submission =test_df.loc[:,['id','target']]
-print(len(submission))
+submission['target'] = submission['target'].apply(lambda x:int(x))
 print(submission)
 
 submission.to_csv('submission.csv',index = False, header=True,encoding='utf-8',sep= ',')
--- main.py
+++ main.py
@@ -8,6 +8,13 @@ from nltk.corpus import stopwords
 from nltk.tokenize import TweetTokenizer
 import pickle
 from nltk.classify import NaiveBayesClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfTransformer
+from gensim.models import Word2Vec
+from sklearn.linear_model import SGDClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 #Import train and test dataset
 train_df = pd.DataFrame(pd.read_csv('data/train.csv',sep = ','))
@@ -45,19 +52,35 @@ def find_feature(word_list):
     return(feature)
 
 document = document.values.tolist()
+#########################
+#Naive Bayes classifier
+# feature_sets = [(find_feature(word_list),target) for (word_list,target) in document]
+#
+# #train_set,test_set = model_selection.train_test_split(feature_sets,test_size = 0.0)
+#
+# model = NaiveBayesClassifier.train(feature_sets)
+#
+# # accuracy = nltk.classify.accuracy(model, test_set)
+# # print('SVC Accuracy : {}%'.format(accuracy*100))
+##################################
+#Pipeline
+X = train_df.loc[:,'processed_tweet']
+Y = train_df.loc[:,'target']
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X,Y,test_size = 0.25)
+sgd = Pipeline([('vect', CountVectorizer()),
+                ('tfidf', TfidfTransformer()),
+                ('clf', SGDClassifier(loss='hinge', penalty='l2',alpha=0.0001, random_state=42, tol=None)),
+               ])
+sgd.fit(X, Y)
 
-feature_sets = [(find_feature(word_list),target) for (word_list,target) in document]
+y_pred = sgd.predict(X_test)
 
-train_set,test_set = model_selection.train_test_split(feature_sets,test_size = 0.15)
-model = NaiveBayesClassifier.train(train_set)
-
-accuracy = nltk.classify.accuracy(model, test_set)
-print('SVC Accuracy : {}%'.format(accuracy*100))
-
-
+print(y_pred[0])
+# print('accuracy %s' % accuracy_score(y_pred, y_test))
+# print(classification_report(y_test, y_pred,target_names=['1','0']))
 # save the model to disk
 filename = 'finalized_model.sav'
-pickle.dump(model, open(filename, 'wb'))
+pickle.dump(sgd, open(filename, 'wb'))
 pickle.dump(feature_vector, open("feature_vector.pickle","wb"))
 print('Saved model to disk')
 
